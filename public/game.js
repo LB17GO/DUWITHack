90a12f4749fd0fc8ba5b285ea6c896244af6d6c9
Lucy Bentham <147002329+LB17GO@users.noreply.github.com>
2025-03-01 21:25:43 +0000

    Rocket added

--- public/game.js
+++ public/game.js
@@ -1,6 +1,29 @@
-function setup() {
-    createCanvas(400, 400);
-}
-function draw() {
-    background(220);
-}
+// Handle click events on rocket parts
+document.getElementById("rocket-body").addEventListener("click", function() {
+    
+});
+  
+document.getElementById("rocket-nose").addEventListener("click", function() {
+
+});
+  
+document.getElementById("rocket-fin-left").addEventListener("click", function() {
+    
+});
+  
+document.getElementById("rocket-fin-right").addEventListener("click", function() {
+    
+});
+  
+document.getElementById("booster-left").addEventListener("click", function() {
+    
+});
+  
+document.getElementById("booster-right").addEventListener("click", function() {
+    
+});
+  
+document.getElementById("rocket-window").addEventListener("click", function() {
+    
+});
+  
